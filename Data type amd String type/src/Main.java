71d6b9a0839c641cc16233c1bc2d481d67860e30
Brianna Zhou <2258658@learn.vsb.bc.ca>
2021-02-11 10:25:17 -0800

    Practice on variables and names
    Date type and String type

---
+++ Data type amd String type/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        int room;
+        double e;
+        room = 113;
+        e = 2.71828;
+        String FirstSubject = "Computer";
+        String SecondSubject = "Science";
+        System.out.println( "This is " + "room # " + room);
+        System.out.println( "e " + "is close to " + e);
+        System.out.println( "I am learning "+ "a bit about " + (FirstSubject + SecondSubject ) );
+
+    }
+}
